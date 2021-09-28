af93419c5ddd3f2e1a6d7046eab49a8e012f7ddf
weak_ptr <weak_ptr@outlook.com>
2021-09-28 16:50:24 +0800

    remove redundant code

--- cm04/packer.cpp
+++ cm04/packer.cpp
@@ -1,21 +1,6 @@
 #include <LIEF/LIEF.hpp>
-#include <fstream>
-#include <iostream>
-#include <memory>
-
-size_t align(size_t n, size_t alignment) {
-  if (n % alignment == 0) {
-    return n;
-  }
-  return n - (n % alignment) + alignment;
-}
-
-void padding(std::vector<uint8_t> &data, size_t alignment) {
-  auto pad = align(data.size(), alignment);
-  if (pad != 0) {
-    data.resize(data.size() + pad, 0);
-  }
-}
+#include <Windows.h>
+#include <vector>
 
 std::vector<uint8_t> read_file(const std::string &path) {
   auto h = CreateFile(path.c_str(), GENERIC_READ, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
@@ -43,17 +28,10 @@ int main(int argc, const char *argv[]) {
   auto program_path = argv[2];
   auto loader_binary = LIEF::PE::Parser::parse(loader_path);
 
-  auto file_alignment = loader_binary->optional_header().file_alignment();
-  auto section_alignment = loader_binary->optional_header().section_alignment();
-
   auto program_content = read_file(program_path);
-  padding(program_content, file_alignment);
   auto packed_section = LIEF::PE::Section(".packed");
   packed_section.content(program_content);
-  packed_section.characteristics(static_cast<uint32_t>(
-      LIEF::PE::SECTION_CHARACTERISTICS::IMAGE_SCN_MEM_READ | LIEF::PE::SECTION_CHARACTERISTICS::IMAGE_SCN_MEM_WRITE |
-      LIEF::PE::SECTION_CHARACTERISTICS::IMAGE_SCN_CNT_INITIALIZED_DATA));
-  loader_binary->add_section(packed_section);
+  loader_binary->add_section(packed_section, LIEF::PE::PE_SECTION_TYPES::DATA);
 
   auto builder = LIEF::PE::Builder::Builder(loader_binary.get());
   builder.build();
